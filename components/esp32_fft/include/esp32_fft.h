38fe3991a3a2dede04006b6fe580d7eae541011e
Barabas Raffai <barabas.raffai@gmail.com>
2024-03-10 18:39:46 +0000

    Add C++ support

--- components/esp32_fft/include/esp32_fft.h
+++ components/esp32_fft/include/esp32_fft.h
@@ -38,39 +38,70 @@
 #ifndef __ESP32_FFT_H__
 #define __ESP32_FFT_H__
 
-typedef enum { ESP32_FFT_REAL, ESP32_FFT_COMPLEX } esp32_fft_type_t;
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-typedef enum { ESP32_FFT_FORWARD, ESP32_FFT_BACKWARD } esp32_fft_direction_t;
+typedef enum
+{
+    ESP32_FFT_REAL,
+    ESP32_FFT_COMPLEX
+} esp32_fft_type_t;
+
+typedef enum
+{
+    ESP32_FFT_FORWARD,
+    ESP32_FFT_BACKWARD
+} esp32_fft_direction_t;
 
 #define ESP32_FFT_OWN_INPUT_MEM 1
 #define ESP32_FFT_OWN_OUTPUT_MEM 2
 
-typedef struct {
-  int size;                        // FFT size
-  float *input;                    // pointer to input buffer
-  float *output;                   // pointer to output buffer
-  float *twiddle_factors;          // pointer to buffer holding twiddle factors
-  esp32_fft_type_t type;           // real or complex
-  esp32_fft_direction_t direction; // forward or backward
-  unsigned int flags;              // FFT flags
+typedef struct
+{
+    int size;               // FFT size
+    float *input;           // pointer to input buffer
+    float *output;          // pointer to output buffer
+    float *twiddle_factors; // pointer to buffer holding twiddle factors
+    esp32_fft_type_t type;  // real or complex
+    esp32_fft_direction_t direction; // forward or backward
+    unsigned int flags;              // FFT flags
 } esp32_fft_config_t;
 
-esp32_fft_config_t *esp32_fft_init(int size, esp32_fft_type_t type,
+esp32_fft_config_t *esp32_fft_init(int size,
+                                   esp32_fft_type_t type,
                                    esp32_fft_direction_t direction,
-                                   float *input, float *output);
+                                   float *input,
+                                   float *output);
 void esp32_fft_destroy(esp32_fft_config_t *config);
 void esp32_fft_execute(esp32_fft_config_t *config);
 void esp32_fft_fft(float *input, float *output, float *twiddle_factors, int n);
 void esp32_fft_ifft(float *input, float *output, float *twiddle_factors, int n);
 void esp32_fft_rfft(float *x, float *y, float *twiddle_factors, int n);
 void esp32_fft_irfft(float *x, float *y, float *twiddle_factors, int n);
-void esp32_fft_fft_primitive(float *x, float *y, int n, int stride,
-                             float *twiddle_factors, int tw_stride);
-void esp32_fft_split_radix_fft(float *x, float *y, int n, int stride,
-                               float *twiddle_factors, int tw_stride);
-void esp32_fft_ifft_primitive(float *input, float *output, int n, int stride,
-                              float *twiddle_factors, int tw_stride);
+void esp32_fft_fft_primitive(float *x,
+                             float *y,
+                             int n,
+                             int stride,
+                             float *twiddle_factors,
+                             int tw_stride);
+void esp32_fft_split_radix_fft(float *x,
+                               float *y,
+                               int n,
+                               int stride,
+                               float *twiddle_factors,
+                               int tw_stride);
+void esp32_fft_ifft_primitive(float *input,
+                              float *output,
+                              int n,
+                              int stride,
+                              float *twiddle_factors,
+                              int tw_stride);
 void esp32_fft_fft8(float *input, int stride_in, float *output, int stride_out);
 void esp32_fft_fft4(float *input, int stride_in, float *output, int stride_out);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // __ESP32_FFT_H__
